60546c823e4c467f7837694f059691725c0e26a7
Diamantis Kourkouzelis <diamantis@endor.ai>
2025-06-13 08:23:27 -0700

    new openapi-key

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     ak_flask_token="ak-sec-1234567890"
+    MY_KEY="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"
     return "<p>Flask says Hello!!</p>"
 
 @app.route("/ditto")
@@ -15,5 +16,3 @@ def ditto():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
-MY_KEY=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
