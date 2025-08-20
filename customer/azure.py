d6738f933eff0cd8432f14b59fa98096c2b4a249
Diamantis Kourkouzelis <diamantis@endor.ai>
2025-08-20 14:44:55 -0700

    More on azure secrets

--- customer/azure.py
+++ customer/azure.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 
+# True positives
+# One
 client_secret = 'L9lpo6rAOzGGfMahvdz8CGw2IcSQfK896oMigVY'
 
 # Two
-SUBSCRIPTION_ID = ""
-
 CLIENT_SECRET="LOuB8S2_7BclHW3--16_g8n.47mDRwQL.Y"
 
-# False positive
+# Three (old format, with Q~)
+const clientSecret = "mno3Q~qwertyuiopasdfghjklzxcvbnm123";
+
+
+# False positives
+# React
 googleClientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET
 
-# Old format
-const clientSecret = "mno3Q~qwertyuiopasdfghjklzxcvbnm123";
+# Sops
+enc: YQDhHzcdbz4XXUKsc3PFkqKjcG_aWaCQTTYCk5A8k5zu4A_7Sfw2DA_ywnBbbIGvnJrzMX6I0mXADGkoJt2VYS3ytPZ-Q4vcYxVrvUhPTR8EprSiu2beLVL4iJjT8SilAUKpzzk3Ev8pnlp9XLS9q7NGLEA6rFG09AdBpPtUVEjtr3yWD0XJDuZiPXlg8iWqfXrNxiEnsPiCqp5siVaRVxPzyoXoCwBVg9jv2FyttTNhPX-HgUvaO1J7QMpbycddcpY-kQx7YgIcbxkI9ct1JZoyNPrpeo1SLg_amLKX0g1ZJ8RoLWeARHf5iJdjeyB_m000QeqxMPZIQ3DNNzT8oQ
+
+slackToken: ENC[AES256_GCM,data:yAysjR0MInY2hisd36mlxqmQBx6vp8I/U3PnPTMX4AkJaJbd0CJ+4D4WNPUirYv6OmnX5BYKZxhJ,iv:GWQdesPV9+Gp84Ewotw0k0V5JlfLKARcYVkkNc9Ler8=,tag:6VosorQO0+L0r1W2T8FHFA==,type:str]
+
+#ENC[AES256_GCM,data:86oxwPkPEf71NrOPk+8dI/vbn9X3APAzJdc=,iv:uxsAMTnCZYUQXoBh3HQbu7SAoC4L5NWNY95XUXzv4dU=,tag:WnZLmCVPZeXXdGCIddB82w==,type:comment]
+
+client_secret: ENC[AES256_GCM,data:UcRAydPg1nE4LrxKqeZsiFxUiYFR2FdKRtbhgvmVLEawuKk=,iv:QOiK3f1gfmtgMpd142D7ppagyDW6iI9FK1A/Vg+aLlw=,tag:+THlzriDZVA7RvWVt7bo8g==,type:str]
+
+githubSecret: ENC[AES256_GCM,data:jVKVhoiqMgoTkLuAKVsYLLtiunCyEA==,iv:gLcgclUsuT23bCBPKEd/VrmSj9OA2S855VYwv0pnVcw=,tag:83rPY0Vgo4m3TF3KtW0BFQ==,type:str]
+
