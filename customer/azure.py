7a0b24967ec313a8a9452d8fdd22b64280bdf6dc
Diamantis Kourkouzelis <diamantis@endor.ai>
2025-08-20 08:18:05 -0700

    More on azure secrets (#22)

--- customer/azure.py
+++ customer/azure.py
@@ -6,3 +6,9 @@ client_secret = 'L9lpo6rAOzGGfMahvdz8CGw2IcSQfK896oMigVY'
 SUBSCRIPTION_ID = ""
 
 CLIENT_SECRET="LOuB8S2_7BclHW3--16_g8n.47mDRwQL.Y"
+
+# False positive
+googleClientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET
+
+# Old format
+const clientSecret = "mno3Q~qwertyuiopasdfghjklzxcvbnm123";
