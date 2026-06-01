69d2faf0f361d5532f945b5c71dd124d92bfb11d
Harsh Vardhan Singh <hvsingh@endor.ai>
2026-06-01 20:00:25 +0530

    Add generated tests for new rules

---
+++ generated/azure-storage.py
@@ -0,0 +1,18 @@
+ConnStr1 = "DefaultEndpointsProtocol=https;AccountName=storagetest123;AccountKey=YutGV0Vlauqsobd5tPWz2AKwHhBXMEWsAH+rSbz0UZUfaMVj1CFrcNQK47ygmrC4vHmc7eOp1LdM+AStk5mMyA==;EndpointSuffix=core.windows.net"
+
+ConnStr2 = "EndpointSuffix=core.windows.net;AccountKey=ldlKgoKPJhRjPJTkaC5c/QNqtu4sVQRc/teGJ0MZHbDYEHdvBV5z8JEfJK+evE87D7U8TzMZ0G2C+ASt2B4ifg==;AccountName=storagetest123;DefaultEndpointsProtocol=http"
+
+storageAccountName = "myprodstorage"
+storageAccountKey = "4UWqfvEH56U3u68R35sG4yK1O70NRNoERc0adFtBIV59RaAO9b9NHrthjjaaiSmul6qfUW/RQDAZUQFtt0eLAA=="
+
+blob_url = "https://anotherprodacct.blob.core.windows.net/container1"
+AccountKey = "q3GOojWXu6KyeWtX837WPDb9k81ZOF55rbSuU/kJGcnDqDnuB5QUJSPz/j06QZhXQldIiTpzlSyQLssc/5uAYw=="
+
+storage_account_name = "snakecasestorage"
+storage_account_key = "K9pMfTbZqLrYxWvUjHsGdCnEoIaB7vC2nQ3wA1xY5zPdRtMgFbVlNkJiHcEwUoSzRqXyTpBvCmKfNhDgLjPiOa=="
+
+azurite_conn = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1"
+
+shortAccountKey = "abc123"
+
+fakeAccountKey = "12345678-1234-1234-1234-123456789012"
